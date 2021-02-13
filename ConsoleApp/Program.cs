c743f2e6a541066a2f0bcc56edcf6ad9372ed52a
Leyla Erpay <leylakzl89@gmail.com>
2021-02-13 18:51:57 +0300

    13şubat rev

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //AddNewDataToDb();
+            
             //DeleteTestForCars(carManager);
             //UpdateTestForCars(carManager);
 
